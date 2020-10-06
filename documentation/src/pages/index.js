c624f13e3727b8a0883ddc668e9bb88a70128cee
Candy Tsai <candy02058912@gmail.com>
2020-10-06 15:31:04 +0800

    change class to className

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -5,28 +5,28 @@ export default function Home() {
   return (
     <>
       <SEO title="Search Sketch" />
-      <body class="text-light">
-        <section class="bg-gradient pt-5 pb-5">
-          <div class="container">
-            <div class="row">
-              <div class="col-12 d-flex flex-row align-items-center justify-content-between">
+      <body className="text-light">
+        <section className="bg-gradient pt-5 pb-5">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 d-flex flex-row align-items-center justify-content-between">
                 <h1>Search Sketch</h1>
                 <a
                   href="https://github.com/candy02058912/search-sketch/releases/latest/download/search-sketch.sketchplugin.zip"
-                  class="btn btn-light"
+                  className="btn btn-light"
                 >
                   Download
                 </a>
               </div>
             </div>
-            <div class="row mt-5">
-              <div class="col-md-8 mx-auto text-center">
+            <div className="row mt-5">
+              <div className="col-md-8 mx-auto text-center">
                 <h1>Search Sketch Searches Text in Any Language</h1>
-                <p class="lead mb-5">
+                <p className="lead mb-5">
                   Find your way through the ever-growing sketch files!
                 </p>
                 <a
-                  class="github-button"
+                  className="github-button"
                   href="https://github.com/candy02058912/search-sketch"
                   data-color-scheme="no-preference: dark; light: dark; dark: dark;"
                   data-size="large"
@@ -37,8 +37,8 @@ export default function Home() {
                 </a>
               </div>
             </div>
-            <div class="row mt-5">
-              <div class="col-md-9 mx-auto">
+            <div className="row mt-5">
+              <div className="col-md-9 mx-auto">
                 <img
                   src="https://search-sketch.hpd.io/preview.gif"
                   width="100%"
@@ -48,11 +48,11 @@ export default function Home() {
           </div>
         </section>
 
-        <footer class="py-5 bg-light">
-          <div class="container">
-            <div class="row">
-              <div class="col-12">
-                <p class="text-center text-dark">
+        <footer className="py-5 bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <p className="text-center text-dark">
                   Made with ❤️ by
                   <a href="https://github.com/candy02058912/search-sketch">
                     candy02058912
@@ -61,8 +61,8 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div class="row my-2">
-              <div class="col-md-4 mx-auto text-muted text-center small-xl">
+            <div className="row my-2">
+              <div className="col-md-4 mx-auto text-muted text-center small-xl">
                 &copy; 2020 Candy Tsai - All Rights Reserved.
               </div>
             </div>
